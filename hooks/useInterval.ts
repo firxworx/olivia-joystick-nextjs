4dd1e2fb4153caef52983119663b237b1529e436
Kevin Firko <kfirko@bitcurve.com>
2021-01-05 22:08:42 -0500

    mostly lifted useInterval() hook implementation

---
+++ hooks/useInterval.ts
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react'
+
+export const useInterval = (callback, delay) => {
+  const savedCallback = useRef<unknown>()
+
+  // save the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [ callback ])
+
+  // set interval
+  useEffect(() => {
+    const tick = () => {
+        if (savedCallback.current && typeof savedCallback.current === 'function') {
+            savedCallback.current()
+        }
+    }
+
+    if (delay !== null) {
+      let id = setInterval(tick, delay)
+      return () => clearInterval(id)
+    }
+  },  [ delay ])
+}
