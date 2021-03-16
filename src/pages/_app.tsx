0a6e5f72a9ea40f7e3087f6f24241005fe5bcfe4
Kevin Firko <kfirko@bitcurve.com>
2021-03-16 00:18:12 -0400

    import tailwind.css and finesse for strict mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/tailwind.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Olivia Joystick</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
