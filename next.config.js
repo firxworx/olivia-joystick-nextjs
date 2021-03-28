77d9b4f28c67a2ec33452ccb521496b23dad947a
Kevin Firko <kfirko@bitcurve.com>
2021-03-28 01:44:30 -0400

    prepare for static build and deploy to web host

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// `trailingSlash` (formerly `exportTrailingSlash` is compatible with page paths + s3 deployment scenario
+// see: https://nextjs.org/docs/api-reference/next.config.js/exportPathMap#adding-a-trailing-slash
+
+module.exports = (phase, { defaultConfig }) => {
+  return {
+    trailingSlash: true,
+    basePath: '/olivia-joystick-nextjs', // for deploy to folder off base subdomain
+  }
+}
