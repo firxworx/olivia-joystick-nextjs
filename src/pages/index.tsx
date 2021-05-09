37511cc1ab3c4182aec46565970f96057703ca51
Kevin Firko <kfirko@bitcurve.com>
2021-05-08 20:08:02 -0400

    implement react-spring powered mode transition on index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react'
+import { useTransition, animated } from 'react-spring'
+
+import { useControllerStore } from '../stores/useControllerStore'
+import { useSpeech } from '../hooks/useSpeech'
 
 import { GridLayout } from '../components/GridLayout'
 import { TelevisionMode } from '../components/modes/TelevisionMode'
 import { SpeechMode } from '../components/modes/SpeechMode'
-import { useSpeech } from '../hooks/useSpeech'
 // import { ThreeFunMode } from '../components/modes/ThreeFunMode'
 // import { ThreeMode2 } from '../components/modes/ThreeMode2'
-import { useControllerStore } from '../stores/useControllerStore'
 
 const modes = [
   { name: 'Television Mode', component: TelevisionMode },
@@ -14,6 +16,10 @@ const modes = [
   // { name: 'Three Fun Mode', component: ThreeFunMode },
   // { name: 'Three Mode Two', component: ThreeMode2 },
 ]
+
+const Mode: React.FC<{ index: number }> = ({ index }) => {
+  return modes[index].component({})
+}
 
 export default function IndexPage() {
   const [currentMode, setCurrentMode] = useState(0)
@@ -34,12 +40,25 @@ export default function IndexPage() {
     }
   }, [controller.altButton])
 
-  const CurrentMode = modes[currentMode].component
+  const transitions = useTransition(currentMode, {
+    config: { duration: 500 }, // config.molasses
+    from: {
+      opacity: 0,
+      transform: 'translate3d(-100%, 0px, 0px)',
+    },
+    enter: { opacity: 1, transform: 'translate3d(0%, 0px, 0px)' },
+    leave: { opacity: 0, position: 'absolute', transform: 'translate3d(100%, 0px, 0px)' },
+    // delay...
+    // onRest: () => ...
+  })
 
   return (
     <GridLayout>
-      <CurrentMode />
-      {/*modes[currentMode].component({})*/}
+      {transitions((styles, modeIndex) => (
+        <animated.div className="w-full h-full" style={{ ...styles }}>
+          <Mode index={modeIndex} />
+        </animated.div>
+      ))}
     </GridLayout>
   )
 }
