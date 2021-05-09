1184db3a955b684fc637d3dbe870bf71c2784851
Kevin Firko <kfirko@bitcurve.com>
2021-05-08 20:56:33 -0400

    TelevisionMode now animates 'channel' change up/down with react-spring useTransition

--- src/components/modes/TelevisionMode.tsx
+++ src/components/modes/TelevisionMode.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
+import { useTransition, animated } from 'react-spring'
 import YouTubePlayer from 'react-player/youtube'
+
 import { episodes } from 'src/data/episodes'
 
 import { useSpeech } from '../../hooks/useSpeech'
@@ -7,10 +9,8 @@ import { useControllerStore } from '../../stores/useControllerStore'
 
 const screens = [...episodes[0]]
 
-// lessons:
-// - the onStart callback/handler works the best for using seekTo()
-// - seekTo() does not work reliably, even in the handlePlayerReady() callback, and even in
-//   an effect triggered via a state changed by that callback
+// react-player lessons:
+// - seekTo() is best called from the onStart callback/handler vs. onReady
 
 export const TelevisionMode: React.FC<{}> = () => {
   const playerRef = useRef<YouTubePlayer>(null)
@@ -20,14 +20,18 @@ export const TelevisionMode: React.FC<{}> = () => {
   const [currentScreen, setCurrentScreen] = useState(0)
   const [screenProgress, setScreenProgress] = useState<Array<number>>(Array.from({ length: screens.length }, () => 0))
 
+  const [transitionDirection, setTransitionDirection] = useState<'up' | 'down' | undefined>(undefined)
+
   const speak = useSpeech()
   const joystick = useControllerStore((state) => state.controller)
 
   const handleNext = () => {
+    setTransitionDirection('up')
     setCurrentScreen((currentScreen + 1) % screens.length)
   }
 
   const handleBack = () => {
+    setTransitionDirection('down')
     setCurrentScreen((currentScreen - 1 + screens.length) % screens.length)
   }
 
@@ -72,24 +76,42 @@ export const TelevisionMode: React.FC<{}> = () => {
     }
   }, [joystick.button, joystick.up, joystick.down, joystick.left, joystick.right])
 
+  const transitions = useTransition(currentScreen, {
+    config: { duration: 500 },
+    from: {
+      opacity: 0,
+      transform: `translate3d(0px, ${transitionDirection === 'up' ? '100%' : '-100%'}, 0px)`,
+    },
+    enter: { opacity: 1, transform: 'translate3d(0px, 0%, 0px)' },
+    leave: {
+      opacity: 0,
+      position: 'absolute',
+      transform: `translate3d(0px, ${transitionDirection === 'up' ? '-100%' : '100%'}, 0px)`,
+    },
+  })
+
   return (
     <>
-      <YouTubePlayer
-        ref={playerRef}
-        url={screens[currentScreen]}
-        playing={isPlayMode}
-        width="100%"
-        height="100%"
-        progressInterval={1000}
-        config={{
-          playerVars: {
-            controls: 0,
-            disablekb: true,
-            cc_lang_pref: 'en',
-          },
-        }}
-        onStart={handlePlayerStarted}
-      />
+      {transitions((styles, currentScreenIndex) => (
+        <animated.div className="w-full h-full" style={{ ...styles }}>
+          <YouTubePlayer
+            ref={playerRef}
+            url={screens[currentScreenIndex]}
+            playing={isPlayMode}
+            width="100%"
+            height="100%"
+            progressInterval={1000}
+            config={{
+              playerVars: {
+                controls: 0,
+                disablekb: true,
+                cc_lang_pref: 'en',
+              },
+            }}
+            onStart={handlePlayerStarted}
+          />
+        </animated.div>
+      ))}
     </>
   )
 }
