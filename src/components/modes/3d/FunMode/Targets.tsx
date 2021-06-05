2cba5cf449f27ed3061fb00d4e2b0e9c9591da46
Kevin Firko <kfirko@bitcurve.com>
2021-06-05 19:43:59 -0400

    refactor 3funmode targets

--- src/components/modes/3d/FunMode/Targets.tsx
+++ src/components/modes/3d/FunMode/Targets.tsx
@@ -27,22 +27,39 @@ export const Cube: React.FC<BodyProps> = (props) => {
   )
 }
 
+const CubePyramid: React.FC = () => {
+  return (
+    <>
+      {/* top */}
+      <Cube position={[0, 1, -5]} />
+      {/* middle l+r */}
+      <Cube position={[-0.3, 0.5, -5]} />
+      <Cube position={[0.3, 0.5, -5]} />
+      {/* bottom l+m+r */}
+      <Cube position={[-0.6, 0, -5]} />
+      <Cube position={[0, 0, -5]} />
+      <Cube position={[0.6, 0, -5]} />
+    </>
+  )
+}
+
+const CubeStiltedStack: React.FC = () => (
+  <>
+    <Cube position={[-5, 0, -5]} />
+    <Cube position={[-5, 0.5, -5]} />
+    <Cube position={[-5, 1, -5]} />
+    <Cube position={[-5, 1.5, -5]} />
+  </>
+)
+
 export const Targets: React.FC<{}> = () => {
   return (
     <>
-      {/* og pyramid */}
-      <Cube position={[0, 1, -5]} />
-      <Cube position={[-0.3, 0.5, -5]} />
-      <Cube position={[0.3, 0.5, -5]} />
-      <Cube position={[-0.6, 0, -5]} /> {/* bottom left */}
-      <Cube position={[0, 0, -5]} /> {/* bottom middle */}
-      <Cube position={[0.6, 0, -5]} /> {/* bottom right */}
-      {/* stack */}
-      <Cube position={[-5, 0, -5]} />
-      <Cube position={[-5, 0.5, -5]} />
-      <Cube position={[-5, 1, -5]} />
-      <Cube position={[-5, 1.5, -5]} />
+      <CubePyramid />
+      <CubeStiltedStack />
+
       {/*
+      // set type Static for cubes that don't move, e.g. walls
       <Cube position={[0, 0, 5]} type={'Static'} />
       <Cube position={[0, 0, 5.5]} type={'Static'} />
       <Cube position={[0, 0.5, 5.5]} type={'Static'} />
