9a84c0ebb0cf1d929731d90391362292b6e2b61c
Kevin Firko <kfirko@bitcurve.com>
2021-06-22 01:17:06 -0400

    ever more episodes to the list - might as well commit em

--- src/data/episodes.ts
+++ src/data/episodes.ts
@@ -7,6 +7,51 @@
  */
 export const episodes = [
   [
+    'https://www.youtube.com/watch?v=MDF8sAnY92Q', // masha girl power
+    'https://www.youtube.com/watch?v=XyYYIL3fsVM', // booba 3d cartoons
+    'https://www.youtube.com/watch?v=VBKgPmS5Fyw', // mickey disney shorts
+    'https://www.youtube.com/watch?v=mbi3vr4iQTw', // pingu school pet
+    'https://www.youtube.com/watch?v=w3Wluvzoggg', // tom & jerry
+    'https://www.youtube.com/watch?v=i2-6UGiTJfg', // pingu loses boat
+    'https://www.youtube.com/watch?v=IaSB0h00mF8', // woody woodpecker
+    'https://www.youtube.com/watch?v=NDYCjMfTOOQ', // looney tunes full episodes remastered compilation
+    'https://www.youtube.com/watch?v=raAXwCFcuLk', // masha dance the night away
+    'https://www.youtube.com/watch?v=OB4gzYZeux8', // sesame 3725
+    'https://www.youtube.com/watch?v=7RiKAnSoV0E', // masha little miss braveheart
+  ],
+  [
+    'https://www.youtube.com/watch?v=-7J_6bQbnl4', // barney
+    'https://www.youtube.com/watch?v=Wiw5kYi1TL0', // 4K Sense of Flying - 1 Hour Relaxing Flight Scenes & Soothing Guitar Music - UHD Aerial Drone Video
+    'https://www.youtube.com/watch?v=aGQt43VIMa4', // Barney's Around the World Adventure - Part 1 (Full Episode)
+    'https://www.youtube.com/watch?v=17TMZ42Hbaw', // Story Time with Josh & Blue 📖 Compilation! | Blue's Clues & You!
+    'https://www.youtube.com/watch?v=mQfD_KU2v60', // Josh & Blue Skidoo Into Books 📖 Compilation! | Blue's Clues & You!
+    'https://www.youtube.com/watch?v=_5YwWZ6b39k', // Peppa Pig Official Channel | Peppa Pig and George's Giant Sunflower
+    'https://www.youtube.com/watch?v=a-0fu7N1O9A', // Slimer, Come Home! | The Real Ghostbusters Ep 4 | Animated Series | GHOSTBUSTERS
+    'https://www.youtube.com/watch?v=5Nj2NQ8Kccg', // Blue's Clues Season 1 Episode 2 What Time Is It For Blue?
+    'https://www.youtube.com/watch?v=2Tb_FvXuGnU', // The Super Simple Show - Cats And Dogs | Kids Songs & Cartoons About Pets
+    'https://www.youtube.com/watch?v=L2D_TgED798', // Stunning 4K Underwater Footage - Relaxing Music With Colorful Underwater Life Video
+  ],
+  [
+    'https://www.youtube.com/watch?v=UhhZFL5b_BM', // dora shy rainbow
+    'https://www.youtube.com/watch?v=5bJow8DmG9s', // Ice Cream Cones | Shaun the Sheep Season 4 | Full Episode
+    'https://www.youtube.com/watch?v=_7S_6-_4j0M', // George at Auction 🐵 Curious George 🐵 WildBrain
+    'https://www.youtube.com/watch?v=n9zErIaBkCg', // Curious George Flies A Kite - Curious George | WildBrain
+    'https://www.youtube.com/watch?v=pJ3eXRM8rQE', // Barney | Come Blow Your Horn! | Full Episode | Season 7
+    'https://www.youtube.com/watch?v=z7LwDvUfR9Q', // Curious George 🐵Water to Ducks 🐵WildBrain
+    'https://www.youtube.com/watch?v=VkqMiKIc6wE', // The Berenstain Bears: Too Much Junk Food/Go to Camp - Ep.13
+    'https://www.youtube.com/watch?v=5yGDP151_bM', // The Mighty Pinkwood Tree | Pink Panther and Pals
+  ],
+  [
+    'https://www.youtube.com/watch?v=ONvNWclIes4', // dora winter
+    'https://www.youtube.com/watch?v=hhLjYIm2XfM', // Sofia The First | Season 1 Episode 2 - The Big Sleepover
+    'https://www.youtube.com/watch?v=HWw3LNB7r5A', // Strawberry Shortcake 🍓The Big Freeze 🍓 Berry Bitty Adventures
+    'https://www.youtube.com/watch?v=3d5JtSHC3vw', // The New Guppy! Full Episode w/ Zooli | Bubble Guppies | Nick Jr.
+    'https://www.youtube.com/watch?v=YB6_FWfNpC4', // Peppa Valentines
+    'https://www.youtube.com/watch?v=dwFeOcNNR4c', // Caillou's broken bone
+    'https://www.youtube.com/watch?v=L50l3xJlyXQ', // Dora the star catcher
+    'https://www.youtube.com/watch?v=t_nhj1w9eaQ', // Masha & Sabre tooth bear
+  ],
+  [
     'https://www.youtube.com/watch?v=-PD1qZjaHO8', // barbie dreamhose s2 e9
     'https://www.youtube.com/watch?v=YszS9RIxJM0', // masha queen of the forest
     'https://www.youtube.com/watch?v=iD07iTS9kmA', // curious george back at the farm
